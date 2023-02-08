56089de47920bedaeec0f4eda0784e6b443cce18
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-02-08 15:06:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 function preload()
-{  
+{
 }
 
 function setup()
 {
-    canvas = createCanvas(800, 500);
-    canvas.position(275, 125);
+  canvas = createCanvas(360, 370);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(360, 360);
+  video.hide();
 
-    video = createCapture(VIDEO);
-    video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+  console.log('O poseNet inicializou');
+}
+
+function gotPoses(results)
+{
+  if (results.length > 0 )
+  {
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+  }
 }
 
 function draw()
 {
-    image(video, 150, 80, 550, 350);
-
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-
-    circle(750, 460, 70);
-    circle(750, 40, 70);
-    circle(50, 40, 70);
-    circle(50, 460, 70);
-
-    fill(0, 255, 100);
-    stroke(255, 255, 100);
-
-    rect(30, 30, 750, 25);
-    rect(33, 25, 25, 450);
-    rect(740, 25, 25, 450);
-    rect(30, 450, 750, 25);
+  image(video,0 ,0, 360, 360)
 }
 
 function takeSnapshot()
 {
-    save('Sua_Foto_Decorada')
+  save ("Bigode.png");
 }
+
