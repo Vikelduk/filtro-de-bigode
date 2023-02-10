8b2f1de219e9baa02c46703c8811318d8369870d
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-02-10 15:51:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+noseX = 0;
+noseY = 0;
+
 function preload()
 {
+  bigode = loadImage('https://i.postimg.cc/pXPFCSb0/bigode.png');
 }
 
 function setup()
 {
-  canvas = createCanvas(360, 370);
+  canvas = createCanvas(350, 350);
   canvas.center();
   video = createCapture(VIDEO);
-  video.size(360, 360);
+  video.size(350, 350);
   video.hide();
 
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -26,12 +30,16 @@ function gotPoses(results)
     console.log(results);
     console.log("nose x = " + results[0].pose.nose.x);
     console.log("nose y = " + results[0].pose.nose.y);
+
+    noseX = results[0].pose.nose.x - 8;
+    noseY = results[0].pose.nose.y + 1;
   }
 }
 
 function draw()
 {
-  image(video,0 ,0, 360, 360)
+  image(video, 0 , 0, 350, 350);
+  image(bigode, noseX, noseY, 60, 60);
 }
 
 function takeSnapshot()
